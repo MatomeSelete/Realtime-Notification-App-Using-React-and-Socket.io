601c52d526447ae436cb0961834bde74e6a595cc
unknown <matome.selete@dadevs.co.za>
2023-02-21 00:33:23 +0200

    commiting

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heart from '../../images/heart.svg'
 import Comment from '../../images/comment.svg'
 import Info from '../../images/info.svg'
-import Message from '../../images/message.svg'
-import Notification from '../../images/notification.svg'
-import Setting from '../../images/settings.svg'
+// import Message from '../../images/message.svg'
+// import Notification from '../../images/notification.svg'
+// import Setting from '../../images/settings.svg'
+import HeartFilled from '../../images/heart-solid.svg'
 import Share from '../../images/share.svg'
-
-
 
 import './Card.css'
 
 function Card({ post }) {
+
+  const [liked, setLiked] = useState(false)
+
+  const handleNotification = ()=> {
+    setLiked(true)
+  }
+
   return (
     <div className='card'>
         <div className='info'>
         <img src={post.userImg} alt='' className='userImg' />
         <span> { post.fullname } </span>
         </div>
-    <img src={ post.postImg } className="postImg" />
-    <div className="interraction">
-        <img src={Heart} className='cardIcon' />
-        <img src={ Comment } className='cardIcon' />
-        <img src={ Share } className='cardIcon' />
-        <img src={ Info } className='cardIcon' />
+    <img src={ post.postImg } alt='' className="postImg" />
+    <div className="interaction">
+
+      {liked? (
+        <img src={ HeartFilled } alt='' className='cardIcon' />
+      ) : (
+        <img src={Heart} alt='' className='cardIcon'  onClick={ handleNotification }/>
+      )}
+        
+        <img src={ Comment } alt='' className='cardIcon' />
+        <img src={ Share } alt='' className='cardIcon' />
+        <img src={ Info } alt='' className='cardIcon infoIcon' />
     </div>
     </div>
   )
